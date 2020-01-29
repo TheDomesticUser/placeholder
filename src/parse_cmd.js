3c77da662c940773217f127707b944687ac28544
TheDomesticUser <TheDomesticUser@gmail.com>
2020-01-29 10:12:23 -0500

    Added improvements to parser and now long and short are fully functional

--- src/parse_cmd.js
+++ src/parse_cmd.js
@@ -5,7 +5,10 @@ const regex = require('xregexp');
 function parseArgs(cmd)
 {
     try {
+        const shortArgs = parseShortArgs(cmd);
         const longArgs = parseLongArgs(cmd);
+
+        console.log(shortArgs);
         console.log(longArgs);
 
     } catch(err) {
@@ -16,13 +19,26 @@ function parseArgs(cmd)
 // Parses all of the short form command options
 function parseShortArgs(cmd)
 {
-    /*  */
     try {
         const matches = [];
-        const re = regex('\\s-(?<short>[a-zA-Z0-9]+)(?=\\s)', 'i');
 
+        // Match the options and arguments, splicing them into the array
+        const re = regex(`
+            \\s-(?<options>[a-z0-9]+)(?!\\S) # Match the options
+            (?:\\s*"(?<arg>[^"]*)")? # Match the arguments
+        `, 'ix');
+        
+        /* Get the options and arguments (If they exist)
+            The supplied argument will be correlated to the last option in the list.
+            Ex. -lm "Message" m -> "Message"
+        */
         regex.forEach(cmd, re, match => {
-            matches.push(match['short']);
+            const options = (match.options).split('');
+            const last = options[options.length - 1];
+
+            const argument = { [last]: match.arg };
+
+            matches.push([options, argument]);
         });
 
         return matches;
@@ -35,23 +51,20 @@ function parseShortArgs(cmd)
 function parseLongArgs(cmd)
 {
     try {
-        const argKey = 'arg';
         const matches = []
         ;
         const re = regex(`
-            \\s--(?<long>[a-zA-Z]+)(?=\\s) # Retrieve the option
-            (?:\\s+"(?<${argKey}>[^"]*)")?
+            \\s--(?<long>[a-z]+)(?!\\S) # Retrieve the option
+            (?:\\s*"(?<arg>[^"]*)")? # Retrieve its argument if it exists
         `, 'ix');
 
-        // Get the option and argument if it exists
+        // Get the option and arguments (If they exist)
         regex.forEach(cmd, re, match => {
             const opt = [];
 
             opt.push(match['long']);
+            opt.push(match['arg']);
 
-            if (match.hasOwnProperty(argKey)) {
-                opt.push(match[argKey]);
-            }
             matches.push(opt);
         });
         
