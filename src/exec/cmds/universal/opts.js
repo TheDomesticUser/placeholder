d7cead444bd46f6d6eaa8ddd553b63a9141daf31
TheDomesticUser <TheDomesticUser@gmail.com>
2020-01-30 16:32:28 -0500

    Added universal option repository

---
+++ src/exec/cmds/universal/opts.js
@@ -0,0 +1,9 @@
+/*
+    Every command will contain these options. They usually redirect stdout
+    and stderr. Please keep in mind that in consequence, the universal option 
+    keywords are reserved, and having the same option name for one separate
+    command will cause ambiguity
+*/
+
+// Universal command names
+const stdoutDmOptName = 'stdout-dm';
