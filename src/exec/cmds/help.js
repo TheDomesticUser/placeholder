b9c094f15df9e786a2610074deafcb3f7a453194
TheDomesticUser <TheDomesticUser@gmail.com>
2020-01-29 20:24:15 -0500

    Added -d --direct option to help cmd

--- src/exec/cmds/help.js
+++ src/exec/cmds/help.js
@@ -1,9 +1,22 @@
 // Provide a url to the documentation
 function help(msg, opts)
 {
-    const url = 'https://github.com/TheDomesticUser/placeholder/tree/master/docs';
+    let url = 'https://github.com/TheDomesticUser/placeholder/tree/master/docs';
+    let sender = msg.channel;
 
-    msg.channel.send(url);
+    // Loop over the options, making modifications to its response properties
+    for (let opt of opts) {
+        console.log(opt[0]);
+        // Find the corresponding option
+        switch(opt[0]) // Option = [0], Argument = [1]
+        {
+            case 'd': case 'direct':
+                sender = msg.author;
+                break;
+        }
+    }
+
+    sender.send(url);
 }
 
 module.exports = help;
