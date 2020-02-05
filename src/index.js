e1289e84cd3c4c8a5aca9f8c5c13058e4478b9fa
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-05 16:46:35 -0500

    The client now prompts the owner to add administrative permissions to it, disabling its functionality if it doesn't.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,15 @@ client.on('message', msg => {
 
     // Don't parse the message if the message wasn't sent in a text channel
     if (msg.channel.type !== 'text') return;
+
+    /*
+        Check if the client has administrative permissions. 
+        Prompt the user if there is not, disabling functionality
+    */
+    if (!client.guild.me.hasPermission('ADMINISTRATOR')) {
+        msg.channel.send('You need to enable administration privileges to the bot!');
+        return;
+    }
     
     const cmd = content.substring(prefix.length).trim();
 
