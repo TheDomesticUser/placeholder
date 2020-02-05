81e2e659d0768a02dbe9bc6726fb19ccadb9e3be
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-04 22:03:47 -0500

    Added prompt when option requirements are not met for

--- src/exec/cmds/send.js
+++ src/exec/cmds/send.js
@@ -4,6 +4,9 @@ const universalOpts = require('./universal/opts.js');
 // Import local variables from other files
 const invalid = require('../../error/invalid.js');
 
+// Import functions for searching
+const search = require('../../../reuse/search.js');
+
 function send(msg, opts)
 {
     let stdout;
@@ -18,20 +21,25 @@ function send(msg, opts)
                 message = opt[1];
                 break;
             case 'u': case 'user':
-                console.log(opt[1]);
+                stdout = search.findMember(msg.guild.members, opt[1]);
                 break;
             // Universal options
             case universalOpts.stderrDmOptName:
                 stderr = msg.author;
                 break;
             default:
-                stderr.send(`Invalid option '${opt[0]}' Try $${invalid.helpCmd} for more information.`);
+                stderr.send(`Invalid option '${opt[0]}' Try '$${invalid.helpCmd} send' for more information.`);
                 break;
         }
     }
+    
+    try {
+        // Sends the requested user the message specified
+        stdout.send(message);
+    } catch(err) {
+        // Prompt the user if required options are not specified
+        stderr.send(`Both the user and the message needs to be specified. Try '$${invalid.helpCmd} send' for more information`);
+    }
 }
 
-/*
-    Finds the user specified through a tag or username,
-    and returns it. Used for the -u --user option.
-*/
+module.exports = send;
